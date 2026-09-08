e2c7bca77bb0e6ba09203523999796197359126c
Amir Tahani <Amirthn653381@gmail.com>
2026-09-08 18:59:51 +0300

    ci: resolve dependency imports, pythonpath configuration, and test suites

--- tests/test_email_assistant.py
+++ tests/test_email_assistant.py
@@ -1,3 +1,5 @@
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 ﻿from utils.helpers import clean_text, format_output
 from gmail.gmail_reader import get_emails
 
