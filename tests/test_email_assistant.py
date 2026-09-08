d73b32d3aec282444c3fa018f51dea7f18c62e5a
Amir Tahani <Amirthn653381@gmail.com>
2026-09-08 19:05:33 +0300

    test: strip UTF-8 BOM from test files and configs

--- tests/test_email_assistant.py
+++ tests/test_email_assistant.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-﻿from utils.helpers import clean_text, format_output
+from utils.helpers import clean_text, format_output
 from gmail.gmail_reader import get_emails
 
 def test_clean_text():
